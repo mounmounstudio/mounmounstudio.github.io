6dedcdbf26f7dddaa419002470ae682bcfdf3afe
xinrulyu <sinrulyu@gmail.com>
2019-05-12 16:47:30 +0800

    add about page

--- js/all.js
+++ js/all.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // NAV
     $("#menuIcon").click(function (e) {
         e.preventDefault();
         $(".mobile_menu").fadeIn();
@@ -6,5 +7,10 @@ $(document).ready(function () {
     $("#closeIcon").click(function (e) {
         e.preventDefault();
         $(".mobile_menu").fadeOut();
+    });
+    //PageLoad
+    $(".animateShow").delay(1000).css({
+        "opacity": "1",
+        "margin-top": "0"
     });
 });
