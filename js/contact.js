dddfdc9e0961c246e1f9ab1245c4bdc66001b0f8
xinrulyu <sinrulyu@gmail.com>
2019-05-19 23:28:35 +0800

    new test

--- js/contact.js
+++ js/contact.js
@@ -1,55 +1,55 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    $(".btnAction").click(function () {
-        var valid;
-        valid = validateContact();
-        if (valid) {
-            jQuery.ajax({
-                url: "mailer.php",
-                data: 'userName=' + $("#userName").val() + '&userEmail=' +
-                    $("#userEmail").val() + '&subject=' +
-                    $("#subject").val() + '&content=' +
-                    $(content).val(),
-                type: "POST",
-                success: function (data) {
-                    $("#mail-status").html(data);
-                },
-                error: function () {}
-            });
-        }
-    });
+//     $(".btnAction").click(function () {
+//         var valid;
+//         valid = validateContact();
+//         if (valid) {
+//             jQuery.ajax({
+//                 url: "mailer.php",
+//                 data: 'userName=' + $("#userName").val() + '&userEmail=' +
+//                     $("#userEmail").val() + '&subject=' +
+//                     $("#subject").val() + '&content=' +
+//                     $(content).val(),
+//                 type: "POST",
+//                 success: function (data) {
+//                     $("#mail-status").html(data);
+//                 },
+//                 error: function () {}
+//             });
+//         }
+//     });
 
 
-    function validateContact() {
-        var valid = true;
-        $(".demoInputBox").css('background-color', '');
-        $(".info").html('');
-        if (!$("#userName").val()) {
-            $("#userName-info").html("(required)");
-            $("#userName").css('background-color', '#FFFFDF');
-            valid = false;
-        }
-        if (!$("#userEmail").val()) {
-            $("#userEmail-info").html("(required)");
-            $("#userEmail").css('background-color', '#FFFFDF');
-            valid = false;
-        }
-        if (!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
-            $("#userEmail-info").html("(invalid)");
-            $("#userEmail").css('background-color', '#FFFFDF');
-            valid = false;
-        }
-        if (!$("#subject").val()) {
-            $("#subject-info").html("(required)");
-            $("#subject").css('background-color', '#FFFFDF');
-            valid = false;
-        }
-        if (!$("#content").val()) {
-            $("#content-info").html("(required)");
-            $("#content").css('background-color', '#FFFFDF');
-            valid = false;
-        }
-        return valid;
-    }
+//     function validateContact() {
+//         var valid = true;
+//         $(".demoInputBox").css('background-color', '');
+//         $(".info").html('');
+//         if (!$("#userName").val()) {
+//             $("#userName-info").html("(required)");
+//             $("#userName").css('background-color', '#FFFFDF');
+//             valid = false;
+//         }
+//         if (!$("#userEmail").val()) {
+//             $("#userEmail-info").html("(required)");
+//             $("#userEmail").css('background-color', '#FFFFDF');
+//             valid = false;
+//         }
+//         if (!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+//             $("#userEmail-info").html("(invalid)");
+//             $("#userEmail").css('background-color', '#FFFFDF');
+//             valid = false;
+//         }
+//         if (!$("#subject").val()) {
+//             $("#subject-info").html("(required)");
+//             $("#subject").css('background-color', '#FFFFDF');
+//             valid = false;
+//         }
+//         if (!$("#content").val()) {
+//             $("#content-info").html("(required)");
+//             $("#content").css('background-color', '#FFFFDF');
+//             valid = false;
+//         }
+//         return valid;
+//     }
 
-});
+// });
--- js/mailer.php
+++ js/mailer.php
@@ -1,9 +1,10 @@
-<?php
-    $toEmail = "nikitasinru@gmail.com";
-    $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
-    if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
-        print "<p class='success'>Mail Sent.</p>";
-    } else {
-        print "<p class='Error'>Problem in Sending Mail.</p>";
-    }
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "nikitasinru@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
