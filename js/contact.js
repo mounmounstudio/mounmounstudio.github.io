403749c39d6523008da05ed2d1731a8818fd94bf
xinrulyu <sinrulyu@gmail.com>
2019-05-19 23:48:05 +0800

    test test again contact

--- js/contact.js
+++ js/contact.js
@@ -1,55 +1,29 @@
-// $(document).ready(function () {
+$(document).ready(function () {
+    //宣告預設表單內容為空 （你想要的話也可以加東西）
+    var initSubject = '',
+        initBody = '';
 
-//     $(".btnAction").click(function () {
-//         var valid;
-//         valid = validateContact();
-//         if (valid) {
-//             jQuery.ajax({
-//                 url: "mailer.php",
-//                 data: 'userName=' + $("#userName").val() + '&userEmail=' +
-//                     $("#userEmail").val() + '&subject=' +
-//                     $("#subject").val() + '&content=' +
-//                     $(content).val(),
-//                 type: "POST",
-//                 success: function (data) {
-//                     $("#mail-status").html(data);
-//                 },
-//                 error: function () {}
-//             });
-//         }
-//     });
+    //按下傳送按鈕後執行
+    function submitHandler() {
+        var to = "nikitasinru@gmail.com"; //寫死的傳送對象 就是公司的信箱 不會顯示在網頁上
+        var name = nameText.value; //讀取ID為 nameTextuser 物件中的值
+        var email = emailText.value;
+        var tel = telText.value;
+        var subject = subText.value;
+        //把user填的資料都塞到 mail body 中
+        var body = "" + bodyText.value + '%0A%0A%0A'; //%0A是換行 換了三行
+        body += "From：" + nameText.value + '%0A';
+        body += "Email：" + emailText.value + '%0A';
+        body += "Tel：" + telText.value;
+        //傳送的主要程式碼
+        mailTo.href = "mailto:" + to + "?subject=" + subject + "&body=" + body;
+        mailTo.click();
+    }
+    //在body onload
+    function init() {
+        subText.value = initSubject;
+        toText.value = initTo;
+        bodyText.value = initBody;
+    }
 
-
-//     function validateContact() {
-//         var valid = true;
-//         $(".demoInputBox").css('background-color', '');
-//         $(".info").html('');
-//         if (!$("#userName").val()) {
-//             $("#userName-info").html("(required)");
-//             $("#userName").css('background-color', '#FFFFDF');
-//             valid = false;
-//         }
-//         if (!$("#userEmail").val()) {
-//             $("#userEmail-info").html("(required)");
-//             $("#userEmail").css('background-color', '#FFFFDF');
-//             valid = false;
-//         }
-//         if (!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
-//             $("#userEmail-info").html("(invalid)");
-//             $("#userEmail").css('background-color', '#FFFFDF');
-//             valid = false;
-//         }
-//         if (!$("#subject").val()) {
-//             $("#subject-info").html("(required)");
-//             $("#subject").css('background-color', '#FFFFDF');
-//             valid = false;
-//         }
-//         if (!$("#content").val()) {
-//             $("#content-info").html("(required)");
-//             $("#content").css('background-color', '#FFFFDF');
-//             valid = false;
-//         }
-//         return valid;
-//     }
-
-// });
+});
