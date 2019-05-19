fc1d521dab1606041b371efab99c21fa0f407fb8
xinrulyu <sinrulyu@gmail.com>
2019-05-19 23:15:07 +0800

    test form

--- js/mailer.php
+++ js/mailer.php
@@ -1,17 +1,9 @@
 <?php
-if($_POST) {
-  $to = "nikitasinru@gmail.com"; // your mail here
-  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-  $tel = filter_var($_POST["tel"], FILTER_SANITIZE_STRING);
-  $subTitle = filter_var($_POST["subTitle"], FILTER_SANITIZE_STRING);
-  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-  $body = "Message: $message\nE-mail: $email\nPhone: $tel";
-  
-  if(@mail($to,  $subTitle, $body)) {
-    $output = json_encode(array('success' => true));
-    die($output);
-  } else {
-    $output = json_encode(array('success' => false));
-    die($output);
-  }
-}
+    $toEmail = "nikitasinru@gmail.com";
+    $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
+    if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
+        print "<p class='success'>Mail Sent.</p>";
+    } else {
+        print "<p class='Error'>Problem in Sending Mail.</p>";
+    }
+?>
