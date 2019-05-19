35ba142f23925dba5ce24e6cee2baa5e91408f3e
xinrulyu <sinrulyu@gmail.com>
2019-05-19 22:19:09 +0800

    add contact php

---
+++ js/contact.js
@@ -0,0 +1,31 @@
+$(document).reday(function () {
+    $("#submit").click(function () {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var tel = $("#tel").val();
+        var subTitile = $("#subTitile").val();
+        var message = $("#message").val();
+
+        alert(name);
+
+        $("#returnmessage").empty(); // To empty previous error/success message.
+        // Checking for blank fields.
+        if (name == '' || email == '' || contact == '') {
+            alert("Please Fill Required Fields");
+        } else {
+            // Returns successful data submission message when the entered information is stored in database.
+            $.post("contact_form.php", {
+                name1: name,
+                email1: email,
+                tel1: tel,
+                subTitile1: subTitile,
+                message1: message
+            }, function (data) {
+                $("#returnmessage").append(data); // Append returned message to message paragraph.
+                if (data == "Your Query has been received, We will contact you soon.") {
+                    $("#form")[0].reset(); // To reset form fields on success.
+                }
+            });
+        }
+    });
+});
