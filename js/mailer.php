5377c4297462991294e2c7ff0ddd7aa931445928
xinrulyu <sinrulyu@gmail.com>
2019-05-21 00:17:12 +0800

    Upload new version

--- js/mailer.php
+++ js/mailer.php
@@ -1,10 +1,14 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "nikitasinru@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+// Fetching Values From URL
+$name2 = $_POST['name1'];
+$email2 = $_POST['email1'];
+$password2 = $_POST['password1'];
+$contact2 = $_POST['contact1'];
+$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server..
+$db = mysql_select_db("mydba", $connection); // Selecting Database
+if (isset($_POST['name1'])) {
+$query = mysql_query("insert into form_element(name, email, password, contact) values ('$name2', '$email2', '$password2','$contact2')"); //Insert Query
+echo "Form Submitted succesfully";
+}
+mysql_close($connection); // Connection Closed
 ?>
