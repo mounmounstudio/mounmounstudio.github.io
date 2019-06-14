05377b199442b11d8977c060c601827d519f4b83
xinrulyu <sinrulyu@gmail.com>
2019-06-14 22:47:32 +0800

    hide contact form

--- js/all.js
+++ js/all.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
     if ($(window).width() < 767) {
-        $(".promoBar").text("6/15 當天下單享八折優惠，一同慶祝 Co-Founder Madeline 的生日！");
+        $(".promoBar").text("6/15 當天全館享八折優惠，一同慶祝 Co-Founder Maddie 的生日！");
     } else {
         $(".promoBar").text("Celebrate our co-founder Madeline's birthday with us! 20% off for all items only on June 15th!");
     }
 
     $(window).resize(function () {
         if ($(window).width() < 767) {
-            $(".promoBar").text("6/15 當天下單享八折優惠，一同慶祝 Co-Founder Madeline 的生日！");
+            $(".promoBar").text("6/15 當天全館享八折優惠，一同慶祝 Co-Founder Maddie 的生日！");
         } else {
             $(".promoBar").text("Celebrate our co-founder Madeline's birthday with us! 20% off for all items only on June 15th!");
         }
