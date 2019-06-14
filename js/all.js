97013ef86be25586dac3b8cfa0f8e106f2c692c5
xinrulyu <sinrulyu@gmail.com>
2019-06-14 21:56:24 +0800

    promo bar update

--- js/all.js
+++ js/all.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    $(".promoBar").text("Celebrate our co-founder Madeline's birthday with us! 20% off for all items only on June 15th!");
+
+    if ($(window).width() < 767) {
+        $(".promoBar").text("6/15 當天下單享八折優惠，一同慶祝 Co-Founder Madeline 的生日！");
+    } else {
+        $(".promoBar").text("Celebrate our co-founder Madeline's birthday with us! 20% off for all items only on June 15th!");
+    }
+
+    $(window).resize(function () {
+        if ($(window).width() < 767) {
+            $(".promoBar").text("6/15 當天下單享八折優惠，一同慶祝 Co-Founder Madeline 的生日！");
+        } else {
+            $(".promoBar").text("Celebrate our co-founder Madeline's birthday with us! 20% off for all items only on June 15th!");
+        }
+    });
+
     // NAV
     $("#menuIcon").click(function (e) {
         e.preventDefault();
